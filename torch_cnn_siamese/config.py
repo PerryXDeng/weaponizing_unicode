74fb97698b3603ed1bdbd88227e962725dfae087
Ryan Missel <rxm7244@rit.edu>
2019-03-21 15:46:28 -0400

    Added rough pytorch implementation

---
+++ torch_cnn_siamese/config.py
@@ -0,0 +1,20 @@
+# for random generator
+SEED = 42069
+
+# dims for input
+IMG_X = 28
+IMG_Y = 28
+NUM_CHANNELS = 1
+
+# constants for normalization of grayscaled inputs into (-0.5, 0.5)
+# Xnorm = (X - (RANGE / 2)) / RANGE
+PIXEL_RANGE = 255
+
+# for training usage, exponential decay with momentum gradients
+DROP_RATE = 0.5
+BATCH_SIZE = 64
+LAMBDA = 5e-4
+BASE_LEARNING_RATE = 0.01
+DECAY_RATE = 0.95
+MOMENTUM = 0.9
+VALIDATION_INTERVAL = 1
