bc5100f8632c03faa6da45adbc4512382d31e35d
PerryXDeng <PerryXDeng@users.noreply.github.com>
2019-04-07 18:53:36 -0400

    added fake data

--- tf_cnn_siamese/data_preparation.py
+++ tf_cnn_siamese/data_preparation.py
@@ -23,3 +23,16 @@ def inputs_placeholders():
                                           conf.IMG_X, conf.IMG_Y, 1), name="x_2")
   labels = tf.placeholder(conf.DTYPE, shape=(conf.BATCH_SIZE, 1), name="labels")
   return x_1, x_2, labels
+
+
+def save_fake_data():
+  x1,x2,y = generate_normalized_data(50000)
+  np.save("fakedata/trainx1.npy", x1)
+  np.save("fakedata/trainx2.npy", x2)
+  np.save("fakedata/trainy.npy", y)
+  x1,x2,y = generate_normalized_data(15000)
+  np.save("fakedata/testx1.npy", x1)
+  np.save("fakedata/testx2.npy", x2)
+  np.save("fakedata/testy.npy", y)
+
+save_fake_data()
