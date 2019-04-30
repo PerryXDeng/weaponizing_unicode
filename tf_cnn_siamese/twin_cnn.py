fd5c7457993d5adc1f9a1527c6b6013e963f16f3
PerryXDeng <PerryXDeng@users.noreply.github.com>
2019-04-30 03:22:53 +0000

    actually fixed validation batch size

--- tf_cnn_siamese/twin_cnn.py
+++ tf_cnn_siamese/twin_cnn.py
@@ -178,10 +178,10 @@ def batch_validate(x1, x2, labels, conv_weights, conv_biases, fc_weights,
   model = construct_full_model(feed_1, feed_2, conv_weights, conv_biases,
                                fc_weights, fc_biases)
   size = x1.shape[0]
-  if size < conf.BATCH_SIZE:
+  if size < conf.VALIDATION_BATCH_SIZE:
     raise ValueError("batch size for validation larger than dataset: %d" % size)
   stats = np.zeros(6)
-  for begin in range(0, size, conf.BATCH_SIZE):
+  for begin in range(0, size, conf.VALIDATION_BATCH_SIZE):
     end = begin + conf.VALIDATION_BATCH_SIZE
     if end <= size:
       stats += calc_stats(session.run(model,
