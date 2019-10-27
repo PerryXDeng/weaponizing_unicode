79cf0115a09d625ae1c88ea89fa664fb6029a249
Quinn Tucker <qxzcraft@gmail.com>
2019-10-27 16:13:07 -0400

    Simplify character image drawing
    Including drawing directly to a single-channel grayscale image.

--- generate_text/generate_character.py
+++ generate_text/generate_character.py
@@ -4,12 +4,6 @@ import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 
 
-def build_28x28_white_image():
-    img = np.zeros((28, 28, 3), np.uint8)
-    img[:] = 255
-    return img
-
-
 def randomize_location(font_obj, chars, out_of_bounds_threshold=0, x_range=28, y_range=28):
     width = font_obj.getsize(chars)[0]
     height = font_obj.getsize(chars)[1]
@@ -23,21 +17,20 @@ def randomize_location(font_obj, chars, out_of_bounds_threshold=0, x_range=28, y
 
 
 
-def drawChar(chars, font_size, font_path, color=(0, 0, 0), base_image=build_28x28_white_image()):
+def drawChar(chars, font_size, font_path, color=0):
     font = ImageFont.truetype(font_path, font_size)
-    random_location_x, random_location_y = randomize_location(font, chars)
-    img_PIL = Image.fromarray(base_image)
+    
+    img_PIL = Image.fromarray(np.full((28, 28), 255, dtype=np.uint8), mode='L')
     draw_PIL = ImageDraw.Draw(img_PIL)
-    coordinates = (random_location_x,random_location_y) # Top-left of character
+    coordinates = randomize_location(font, chars) # Top-left of character
     draw_PIL.text(coordinates, chars, font=font, fill=color)
-    base_image = np.array(img_PIL)
-    return base_image
+    return np.array(img_PIL)
 
 
 
 # Testing first on just showing a single tensor, then will work on iteration
 def transformImg(img):
-    rows, cols, ch = img.shape
+    rows, cols = img.shape
 
     # Creates 3 random start and end points for the transformation
     randStart = np.float32([[0, 0], [0, 27], [27, 27]])
@@ -60,24 +53,16 @@ def transformTensor(tensor):
         tensor[i] = transformImg(tensor[i])
     return tensor
 
-# turns ..., 28, 28, 3 to ..., 28, 28, 1
-def rgb2gray(rgb):
-    return np.reshape(np.dot(rgb[...,:3], [0.2125, 0.7154, 0.0721]), (list(rgb.shape[:-1]) + [1]))
-
 
 def main():
     fontPath = "../fonts/ARIALUNI.ttf"
     img = drawChar(u"\u279D", 12, fontPath)
-    Image.fromarray(img, mode='RGB').show()
-    img = rgb2gray(img)
-
-    img = np.reshape(img, (28, 28))
-    img.astype(np.int8, copy=False)
-    img = np.reshape(img, (28, 28))
-    #Image.fromarray(img, mode='L').show()
-    return
+    img = transformImg(img)
+    
+    assert img.shape == (28, 28)
+    assert img.dtype == np.uint8
+    Image.fromarray(np.squeeze(img)).show()
 
 
 if __name__ == "__main__":
     main()
-
