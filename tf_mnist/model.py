Implemented vectorized cos triplet loss. Wrote custom training loader. Running into bug where no gradients are being found.

tf_mnist/model.py
@@ -2,13 +2,15 @@ import efficientnet.keras as efn
 import numpy as np
 import tf_mnist.generate_datasets as gd
 import cv2 as cv
-import math
 import tensorflow as tf
 
 
-# Returns the cos similarity between two vectors
+def einsum(a, b):
+    return np.einsum('ji,ji->i', a, b)
+
+
 def cos_sim(x1, x2):
-    return np.dot(x1, x2) / (np.linalg.norm(x1) * np.linalg.norm(x2))
+    return einsum(x1, x2) / (np.sqrt(einsum(x1, x1)) * np.sqrt(einsum(x2, x2)))
 
 
 # Where x1 is an anchor input, x2 belongs to the same class and x3 belongs to a different class
@@ -31,17 +33,24 @@ batch_size = 100
 n_epochs = 100
 for epoch in range(n_epochs):
     anchors, positives, negatives, x_1_test, x_2_test, y_test = gd.compile_triplet_datasets()
+    anchors_converted = np.asarray([cv.cvtColor(v, cv.COLOR_GRAY2RGB) for v in anchors])
+    positives_converted = np.asarray([cv.cvtColor(v, cv.COLOR_GRAY2RGB) for v in positives])
+    negatives_converted = np.asarray([cv.cvtColor(v, cv.COLOR_GRAY2RGB) for v in negatives])
+    print(model.trainable_weights)
     epoch_loss = 0
+    # (1, 224, 224, 3)
     for batch in range(54210 // batch_size):
         with tf.GradientTape() as tape:
             batch_start = batch_size * batch
-            anchor_forward, positive_forward, negative_forward = model(anchors[batch_start:batch_start + batch_size]), model(
-                positives[batch_start:batch_start + batch_size]), model(negatives[batch_start:batch_start + batch_size])
-            loss = cos_triplet_loss(anchor_forward,positive_forward,negative_forward)
-            print(loss.shape)
-            # Get gradients of loss wrt the weights.
-            gradients = tape.gradient(loss, model.trainable_weights)
+            anchor_forward, positive_forward, negative_forward = model(
+                anchors_converted[batch_start:batch_start + batch_size]), model(
+                positives_converted[batch_start:batch_start + batch_size]), model(
+                negatives_converted[batch_start:batch_start + batch_size])
+            loss = cos_triplet_loss(anchor_forward, positive_forward, negative_forward)
+            loss_avg = tf.convert_to_tensor(loss)
+        print(loss_avg)
+        # Get gradients of loss wrt the weights.
+        gradients = tape.gradient(loss_avg, model.trainable_weights)
 
-            # Update the weights of the model.
-            optimizer.apply_gradients(zip(gradients, model.trainable_weights))
-
+        # Update the weights of the model.
+        optimizer.apply_gradients(zip(gradients, model.trainable_weights))