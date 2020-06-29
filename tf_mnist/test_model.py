21842c77b02695f246397c30ca73442328c69443
clinsky <linskycooper@gmail.com>
2020-06-29 18:22:06 -0400

    Implemented dataset generator for unicodes of various fonts for triplet loss

--- tf_mnist/test_model.py
+++ tf_mnist/test_model.py
@@ -4,10 +4,11 @@ import tensorflow as tf
 import tf_mnist.train_model as Utils
 import numpy as np
 
+
 def test_model():
     anchors, positives, negatives, x_1_test, x_2_test, y_test = gd.compile_triplet_datasets()
-    x_1_test_model = Utils.data_preprocess(x_1_test,Utils.GRAY,Utils.NORMALIZE)
-    x_2_test_model = Utils.data_preprocess(x_2_test,Utils.GRAY,Utils.NORMALIZE)
+    x_1_test_model = Utils.data_preprocess(x_1_test, Utils.GRAY, Utils.NORMALIZE)
+    x_2_test_model = Utils.data_preprocess(x_2_test, Utils.GRAY, Utils.NORMALIZE)
     y_test = tf.squeeze(tf.convert_to_tensor(y_test, dtype=tf.int32))
     json_file = open('model/model.json', 'r')
     loaded_model_json = json_file.read()
@@ -18,14 +19,14 @@ def test_model():
     confidence_threshold = .5
     correct = 0
     for i in range(17820 // batch_size):
-        start = i*batch_size
-        a = loaded_model.predict(x_1_test_model[start:start+batch_size])
-        b = loaded_model.predict(x_2_test_model[start:start+batch_size])
-        cos_sim = Utils.cos_sim(a,b)
-        predictions = tf.cast(cos_sim > confidence_threshold,dtype=tf.int32)
-        correct_predictions_np = tf.cast(predictions == y_test[start:start+batch_size],dtype=tf.int8).numpy()
+        start = i * batch_size
+        a = loaded_model.predict(x_1_test_model[start:start + batch_size])
+        b = loaded_model.predict(x_2_test_model[start:start + batch_size])
+        cos_sim = Utils.cos_sim(a, b)
+        predictions = tf.cast(cos_sim > confidence_threshold, dtype=tf.int32)
+        correct_predictions_np = tf.cast(predictions == y_test[start:start + batch_size], dtype=tf.int8).numpy()
         correct += np.sum(correct_predictions_np)
-    print(f"Model accuracy, with a confidence threshold of {confidence_threshold}, is {round((correct/17820)*100,2)}%")
+    print(f"Model accuracy, with a confidence threshold of {confidence_threshold}, is {round((correct / 17820) * 100, 2)}%")
 
 
 if __name__ == '__main__':
---
+++ tf_unicode/generate_datasets.py
@@ -0,0 +1,38 @@
+import generate_text.generate_character as draw_char
+import pickle
+import numpy as np
+import random
+
+FONTS_PATH = '../fonts/'
+FONT_SIZE = 0.7
+
+def compile_datasets(dataset_size):
+    infile = open(FONTS_PATH + 'multifont_mapping.pkl', 'rb')
+    unicode_mapping_dict = pickle.load(infile)
+    unicode_count = len(unicode_mapping_dict)
+    infile.close()
+    unicode_chars_anchors = list(unicode_mapping_dict.keys())
+    unicode_chars_samples = list(unicode_mapping_dict.keys())
+    anchors = np.empty((dataset_size,28,28), dtype=np.uint8)
+    positives = np.empty((dataset_size,28,28), dtype=np.uint8)
+    negatives = np.empty((dataset_size,28,28), dtype=np.uint8)
+    for i in range(dataset_size):
+        print(i)
+        anchor_char = unicode_chars_anchors[random.randint(0, len(unicode_chars_anchors))]
+        unicode_chars_anchors.remove(anchor_char)
+        negative_char = anchor_char
+        while negative_char == anchor_char:
+            negative_char = unicode_chars_samples[random.randint(0, unicode_count - 1)]
+        supported_anchor_fonts = unicode_mapping_dict[anchor_char]
+        supported_negative_fonts = unicode_mapping_dict[negative_char]
+        anchor_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
+        positive_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
+        negative_font = FONTS_PATH + supported_negative_fonts[random.randint(0, len(supported_negative_fonts) - 1)]
+        anchors[i] = draw_char.transformImg(draw_char.drawChar(28, chr(anchor_char), FONT_SIZE, anchor_font))
+        positives[i] = draw_char.transformImg(draw_char.drawChar(28, chr(anchor_char), FONT_SIZE, positive_font))
+        negatives[i] = draw_char.transformImg(draw_char.drawChar(28, chr(negative_char), FONT_SIZE, negative_font))
+    return anchors, positives, negatives
+
+
+if __name__ == '__main__':
+    anchors, positives, negatives = compile_datasets(3)
