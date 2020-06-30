e94094e096ff7f3b6f9b1802a1214d6beb9f6c2d
clinsky <linskycooper@gmail.com>
2020-06-29 20:27:56 -0400

    Current data generator requires drawing images at support sizes, and than scaling down.

--- tf_unicode/generate_datasets.py
+++ tf_unicode/generate_datasets.py
@@ -4,7 +4,8 @@ import numpy as np
 import random
 
 FONTS_PATH = '../fonts/'
-FONT_SIZE = 0.7
+FONT_SIZE = .6
+IMAGESIZE = 100
 
 def compile_datasets(dataset_size):
     infile = open(FONTS_PATH + 'multifont_mapping.pkl', 'rb')
@@ -13,9 +14,9 @@ def compile_datasets(dataset_size):
     infile.close()
     unicode_chars_anchors = list(unicode_mapping_dict.keys())
     unicode_chars_samples = list(unicode_mapping_dict.keys())
-    anchors = np.empty((dataset_size,28,28), dtype=np.uint8)
-    positives = np.empty((dataset_size,28,28), dtype=np.uint8)
-    negatives = np.empty((dataset_size,28,28), dtype=np.uint8)
+    anchors = np.empty((dataset_size, IMAGESIZE, IMAGESIZE), dtype=np.uint8)
+    positives = np.empty((dataset_size, IMAGESIZE, IMAGESIZE), dtype=np.uint8)
+    negatives = np.empty((dataset_size, IMAGESIZE, IMAGESIZE), dtype=np.uint8)
     for i in range(dataset_size):
         print(i)
         anchor_char = unicode_chars_anchors[random.randint(0, len(unicode_chars_anchors))]
@@ -28,11 +29,11 @@ def compile_datasets(dataset_size):
         anchor_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
         positive_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
         negative_font = FONTS_PATH + supported_negative_fonts[random.randint(0, len(supported_negative_fonts) - 1)]
-        anchors[i] = draw_char.transformImg(draw_char.drawChar(28, chr(anchor_char), FONT_SIZE, anchor_font))
-        positives[i] = draw_char.transformImg(draw_char.drawChar(28, chr(anchor_char), FONT_SIZE, positive_font))
-        negatives[i] = draw_char.transformImg(draw_char.drawChar(28, chr(negative_char), FONT_SIZE, negative_font))
+        anchors[i] = draw_char.transformImg(draw_char.drawChar(IMAGESIZE, chr(anchor_char), FONT_SIZE, anchor_font))
+        positives[i] = draw_char.transformImg(draw_char.drawChar(IMAGESIZE, chr(anchor_char), FONT_SIZE, positive_font))
+        negatives[i] = draw_char.transformImg(draw_char.drawChar(IMAGESIZE, chr(negative_char), FONT_SIZE, negative_font))
     return anchors, positives, negatives
 
 
 if __name__ == '__main__':
-    anchors, positives, negatives = compile_datasets(3)
+    anchors, positives, negatives = compile_datasets(500)
