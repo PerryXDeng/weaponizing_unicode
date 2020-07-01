be41526727e4708216cd4aa5f3fca3e5a8385a90
clinsky <linskycooper@gmail.com>
2020-07-01 18:16:55 -0400

    Testing data is now generated

--- tf_unicode/generate_datasets.py
+++ tf_unicode/generate_datasets.py
@@ -5,36 +5,52 @@ import random
 import cv2 as cv
 
 FONTS_PATH = '../fonts/'
-FONT_SIZE = 0.2
-IMAGESIZE = 400
 
-def compile_datasets(dataset_size):
+
+def compile_datasets(training_size, test_size, font_size=.2, img_size=400):
     infile = open(FONTS_PATH + 'multifont_mapping.pkl', 'rb')
     unicode_mapping_dict = pickle.load(infile)
     unicode_count = len(unicode_mapping_dict)
     infile.close()
-    unicode_chars_anchors = list(unicode_mapping_dict.keys())
-    unicode_chars_samples = list(unicode_mapping_dict.keys())
-    anchors = np.empty((dataset_size, IMAGESIZE, IMAGESIZE), dtype=np.uint8)
-    positives = np.empty((dataset_size, IMAGESIZE, IMAGESIZE), dtype=np.uint8)
-    negatives = np.empty((dataset_size, IMAGESIZE, IMAGESIZE), dtype=np.uint8)
-    for i in range(dataset_size):
-        anchor_char = unicode_chars_anchors[random.randint(0, len(unicode_chars_anchors)-1)]
-        unicode_chars_anchors.remove(anchor_char)
+    unicode_chars_available = list(unicode_mapping_dict.keys())
+    unicode_chars_population = list(unicode_mapping_dict.keys())
+    anchors = np.empty((training_size, img_size, img_size), dtype=np.uint8)
+    positives = np.empty((training_size, img_size, img_size), dtype=np.uint8)
+    negatives = np.empty((training_size, img_size, img_size), dtype=np.uint8)
+    x1_test = np.empty((test_size, img_size, img_size), dtype=np.uint8)
+    x2_test = np.empty((test_size, img_size, img_size), dtype=np.uint8)
+    y_test = np.arange(test_size) % 2
+    for i in range(training_size):
+        anchor_char = unicode_chars_available[random.randint(0, len(unicode_chars_available) - 1)]
+        unicode_chars_available.remove(anchor_char)
         negative_char = anchor_char
         while negative_char == anchor_char:
-            negative_char = unicode_chars_samples[random.randint(0, unicode_count - 1)]
+            negative_char = unicode_chars_population[random.randint(0, unicode_count - 1)]
         supported_anchor_fonts = unicode_mapping_dict[anchor_char]
         supported_negative_fonts = unicode_mapping_dict[negative_char]
         anchor_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
         positive_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
         negative_font = FONTS_PATH + supported_negative_fonts[random.randint(0, len(supported_negative_fonts) - 1)]
-        anchors[i] = draw_char.transformImg(draw_char.drawChar(IMAGESIZE, chr(anchor_char), FONT_SIZE, anchor_font))
-        positives[i] = draw_char.transformImg(draw_char.drawChar(IMAGESIZE, chr(anchor_char), FONT_SIZE, positive_font))
-        negatives[i] = draw_char.transformImg(draw_char.drawChar(IMAGESIZE, chr(negative_char), FONT_SIZE, negative_font))
-    return anchors, positives, negatives
+        anchors[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(anchor_char), font_size, anchor_font))
+        positives[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(anchor_char), font_size, positive_font))
+        negatives[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(negative_char), font_size, negative_font))
+    for i in range(test_size):
+        x1_char = unicode_chars_available[random.randint(0, len(unicode_chars_available) - 1)]
+        unicode_chars_available.remove(x1_char)
+        x2_char = x1_char
+        if y_test[i] == 0:
+            while x2_char == x1_char:
+                x2_char = unicode_chars_population[random.randint(0, unicode_count - 1)]
+        supported_x1_fonts = unicode_mapping_dict[x1_char]
+        supported_x2_fonts = unicode_mapping_dict[x2_char]
+        x1_font = FONTS_PATH + supported_x1_fonts[random.randint(0, len(supported_x1_fonts) - 1)]
+        x2_font = FONTS_PATH + supported_x2_fonts[random.randint(0, len(supported_x2_fonts) - 1)]
+        x1_test[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(x1_char), font_size, x1_font))
+        x2_test[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(x2_char), font_size, x2_font))
+    return anchors, positives, negatives, x1_test, x2_test, y_test
 
-def test():
+
+def test(font_size=.2, img_size=400):
     # 17 corrupt unicode chars
     # 3600 invalid pixel size
     infile = open(FONTS_PATH + 'multifont_mapping.pkl', 'rb')
@@ -42,20 +58,21 @@ def test():
     infile.close()
     for i in unicode_mapping_dict.keys():
         try:
-            a = random.randint(0,len(unicode_mapping_dict[i])-1)
-            #print(a)
-            draw_char.transformImg(draw_char.drawChar(IMAGESIZE, chr(i), FONT_SIZE, FONTS_PATH + unicode_mapping_dict[i][a]))
-        except (ValueError,OSError) as e:
-            print(e,i,unicode_mapping_dict[i][a])
+            a = random.randint(0, len(unicode_mapping_dict[i]) - 1)
+            # print(a)
+            draw_char.transformImg(
+                draw_char.drawChar(img_size, chr(i), font_size, FONTS_PATH + unicode_mapping_dict[i][a]))
+        except (ValueError, OSError) as e:
+            print(e, i, unicode_mapping_dict[i][a])
 
 
 if __name__ == '__main__':
-    dataset_size = 5000
-    anchors, positives, negatives = compile_datasets(dataset_size)
-    for i in range(dataset_size):
-        cv.imshow('anchor',anchors[i])
-        cv.imshow('positive',positives[i])
-        cv.imshow('negative',negatives[i])
+    training_size = 500
+    test_size = 1000
+    anchors, positives, negatives, x1_test, x2_test, y_test = compile_datasets(training_size, test_size)
+    for i in range(test_size):
+        cv.imshow('x1', x1_test[i])
+        cv.imshow('x2', x2_test[i])
+        print(y_test[i])
         cv.waitKey(0)
         cv.destroyAllWindows()
-
