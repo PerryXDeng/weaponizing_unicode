65ffcb11c38d771b50a277c992614d6fab989b7f
clinsky <linskycooper@gmail.com>
2020-07-03 00:24:07 -0400

    Data pipeline now complete!!!

--- tf_unicode/generate_datasets.py
+++ tf_unicode/generate_datasets.py
@@ -7,9 +7,32 @@ import cv2 as cv
 FONTS_PATH = '../fonts/'
 
 
-def compile_datasets(training_size, test_size, font_size=.2, img_size=400):
+def try_draw_char(char, available_fonts, empty_image, img_size, font_size):
+    if len(available_fonts) == 0:
+        # No fonts support drawing this unicode character, or the unicode character is corrupt!
+        return empty_image
+    else:
+        selected_font = available_fonts[random.randint(0, len(available_fonts) - 1)]
+        try:
+            img = draw_char.transformImg(draw_char.drawChar(img_size, chr(char), font_size, FONTS_PATH + selected_font))
+        except ValueError:
+            # The drawn character is to big for the desired region!
+            available_fonts.remove(selected_font)
+            return try_draw_char(char, available_fonts, empty_image, img_size, font_size)
+        if (img == empty_image).all():
+            # The selected font does not support drawing this character!
+            available_fonts.remove(selected_font)
+            return try_draw_char(char, available_fonts, empty_image, img_size, font_size)
+        else:
+            return img
+
+
+def compile_datasets(training_size, test_size, font_size=.2, img_size=200, color_format='gray'):
+    empty_image = np.full((img_size, img_size), 255)
     infile = open(FONTS_PATH + 'multifont_mapping.pkl', 'rb')
     unicode_mapping_dict = pickle.load(infile)
+    print(len(unicode_mapping_dict[63609]))
+    print(len(unicode_mapping_dict[63656]))
     unicode_count = len(unicode_mapping_dict)
     infile.close()
     unicode_chars_available = list(unicode_mapping_dict.keys())
@@ -21,33 +44,47 @@ def compile_datasets(training_size, test_size, font_size=.2, img_size=400):
     x2_test = np.empty((test_size, img_size, img_size), dtype=np.uint8)
     y_test = np.arange(test_size) % 2
     for i in range(training_size):
-        anchor_char = unicode_chars_available[random.randint(0, len(unicode_chars_available) - 1)]
-        unicode_chars_available.remove(anchor_char)
-        negative_char = anchor_char
-        while negative_char == anchor_char:
-            negative_char = unicode_chars_population[random.randint(0, unicode_count - 1)]
-        supported_anchor_fonts = unicode_mapping_dict[anchor_char]
-        supported_negative_fonts = unicode_mapping_dict[negative_char]
-        anchor_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
-        positive_font = FONTS_PATH + supported_anchor_fonts[random.randint(0, len(supported_anchor_fonts) - 1)]
-        negative_font = FONTS_PATH + supported_negative_fonts[random.randint(0, len(supported_negative_fonts) - 1)]
-        anchors[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(anchor_char), font_size, anchor_font))
-        positives[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(anchor_char), font_size, positive_font))
-        negatives[i] = draw_char.transformImg(
-            draw_char.drawChar(img_size, chr(negative_char), font_size, negative_font))
+        print(i)
+        anchors[i] = empty_image
+        positives[i] = empty_image
+        negatives[i] = empty_image
+        while (anchors[i] == empty_image).all():
+            anchor_char = unicode_chars_available[random.randint(0, len(unicode_chars_available) - 1)]
+            unicode_chars_available.remove(anchor_char)
+            supported_anchor_fonts = unicode_mapping_dict[anchor_char]
+            anchors[i] = try_draw_char(anchor_char, supported_anchor_fonts, empty_image, img_size, font_size)
+        while (negatives[i] == empty_image).all():
+            negative_char = anchor_char
+            while negative_char == anchor_char:
+                negative_char = unicode_chars_population[random.randint(0, unicode_count - 1)]
+            supported_negative_fonts = unicode_mapping_dict[negative_char]
+            negatives[i] = try_draw_char(negative_char, supported_negative_fonts, empty_image, img_size, font_size)
+        while (positives[i] == empty_image).all():
+            # Possible fonts need to be regenerated because the drawing function is bugged
+            supported_positive_fonts = unicode_mapping_dict[anchor_char]
+            print(anchor_char, len(supported_positive_fonts))
+            positives[i] = try_draw_char(anchor_char, supported_positive_fonts, empty_image, img_size, font_size)
     for i in range(test_size):
-        x1_char = unicode_chars_available[random.randint(0, len(unicode_chars_available) - 1)]
-        unicode_chars_available.remove(x1_char)
-        x2_char = x1_char
-        if y_test[i] == 0:
-            while x2_char == x1_char:
-                x2_char = unicode_chars_population[random.randint(0, unicode_count - 1)]
-        supported_x1_fonts = unicode_mapping_dict[x1_char]
-        supported_x2_fonts = unicode_mapping_dict[x2_char]
-        x1_font = FONTS_PATH + supported_x1_fonts[random.randint(0, len(supported_x1_fonts) - 1)]
-        x2_font = FONTS_PATH + supported_x2_fonts[random.randint(0, len(supported_x2_fonts) - 1)]
-        x1_test[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(x1_char), font_size, x1_font))
-        x2_test[i] = draw_char.transformImg(draw_char.drawChar(img_size, chr(x2_char), font_size, x2_font))
+        print(i)
+        x1_test[i] = empty_image
+        x2_test[i] = empty_image
+        while (x1_test[i] == empty_image).all():
+            x1_char = unicode_chars_available[random.randint(0, len(unicode_chars_available) - 1)]
+            unicode_chars_available.remove(x1_char)
+            supported_x1_fonts = unicode_mapping_dict[x1_char]
+            x1_test[i] = try_draw_char(x1_char, supported_x1_fonts, empty_image, img_size, font_size)
+        if y_test[i] == 1:
+            while (x2_test[i] == empty_image).all():
+                # Possible fonts need to be regenerated because the drawing function is bugged
+                supported_x2_fonts = unicode_mapping_dict[x1_char]
+                x2_test[i] = try_draw_char(x1_char, supported_x2_fonts, empty_image, img_size, font_size)
+        else:
+            while (x2_test[i] == empty_image).all():
+                x2_char = x1_char
+                while x2_char == x1_char:
+                    x2_char = unicode_chars_population[random.randint(0, unicode_count - 1)]
+                supported_x2_fonts = unicode_mapping_dict[x2_char]
+                x2_test[i] = try_draw_char(x2_char, supported_x2_fonts, empty_image, img_size, font_size)
     return anchors, positives, negatives, x1_test, x2_test, y_test
 
 
@@ -85,7 +122,7 @@ def display_chars(display_train, display_test, font_size=.2, img_size=400):
 
 if __name__ == '__main__':
     # Tests drawing each unicode character with a random font
-    #test_drawing(.4,200)
+    # test_drawing(.4,200)
 
-    # With OpenCV, display 10 training triplets and 10 testing pairs
-    display_chars(10, 10, .4, 200)
+    # With OpenCV, display 10 training triplets and 5 testing pairs
+    display_chars(10, 5, .4, 200)
--- tf_unicode/train_model.py
+++ tf_unicode/train_model.py
@@ -1,16 +1,15 @@
 import argparse
 import numpy as np
-import efficientnet.keras as efn
-import numpy as np
-import tf_unicode.generate_datasets as data_pipeline
 import cv2 as cv
 import tensorflow as tf
 import tensorflow.keras as K
+import tf_unicode.generate_datasets as data_pipeline
+import efficientnet.keras as efn
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-trsi', '--training_set_iterations', action='store', type=int, default=10)
-parser.add_argument('-trss', '--training_set_size', action='store', type=int, default=5000)
-parser.add_argument('-tess', '--testing_set_size', action='store', type=int, default=1000)
+parser.add_argument('-trsi', '--training_set_iterations', action='store', type=int, default=5)
+parser.add_argument('-trss', '--training_set_size', action='store', type=int, default=500)
+parser.add_argument('-tess', '--testing_set_size', action='store', type=int, default=100)
 parser.add_argument('-bs', '--batch_size', action='store', type=int, default=128)
 
 # Type of global pooling applied to the output of the last convolutional layer, giving a 2D tensor
@@ -24,6 +23,8 @@ parser.add_argument('-lr', '--learning_rate', action='store', type=float, defaul
 parser.add_argument('-lf', '--loss_function', action='store', type=str, default='cos')
 
 parser.add_argument('-s', '--save_model', action='store', type=bool, default=False)
+parser.add_argument('-img', '--img_size', action='store', type=int, default=200)
+parser.add_argument('-font', '--font_size', action='store', type=float, default=.2)
 args = parser.parse_args()
 
 
@@ -48,25 +49,30 @@ def euc_triplet_loss(x1, x2, x3, c):
     return tf.math.maximum(0, tf.reduce_mean(tf.norm((x1 - x2) + 1e-5) - tf.norm((x1 - x3) + 1e-5)) + c)
 
 
-def train(loss_function):
-    model = efn.EfficientNetB4(weights='imagenet', input_tensor=tf.keras.layers.Input([28, 28]), include_top=False,
+def train(loss_function=cos_triplet_loss):
+    model = efn.EfficientNetB4(weights='imagenet',
+                               input_tensor=tf.keras.layers.Input([args.img_size, args.img_size, 3]), include_top=False,
                                pooling=args.pooling)
     # Training Settings
-    optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr)
-    training_iterations = args.trss // args.bs
-    testing_iterations = args.tess // 200
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+    training_iterations = args.training_set_size // args.batch_size
+    testing_iterations = args.testing_set_size // 200
     # Training Loop
-    for epoch in range(args.epochs):
-        anchors, positives, negatives, x1_test, x2_test, y_test = data_pipeline.compile_datasets(args.trss, args.tess)
-        training_set = tf.data.Dataset.from_tensor_slices((anchors, positives, negatives)).batch(args.bs,
+    for epoch in range(args.training_set_iterations):
+        anchors, positives, negatives, x1_test, x2_test, y_test = data_pipeline.compile_datasets(args.training_set_size,
+                                                                                                 args.testing_set_size,
+                                                                                                 font_size=args.font_size,
+                                                                                                 img_size=args.img_size)
+        training_set = tf.data.Dataset.from_tensor_slices((anchors, positives, negatives)).batch(args.batch_size,
                                                                                                  drop_remainder=True)
+
         testing_set = tf.data.Dataset.from_tensor_slices((x1_test, x2_test, y_test)).batch(200, drop_remainder=True)
-        epoch_train_loss = 0
+        epoch_train_loss = tf.convert_to_tensor(0, dtype=tf.float32)
         for training_batch in training_set:
             with tf.GradientTape() as tape:
                 anchor_forward, positive_forward, negative_forward = model(training_batch[0]), model(
                     training_batch[1]), model(training_batch[2])
-                loss = cos_triplet_loss(anchor_forward, positive_forward, negative_forward)
+                loss = loss_function(anchor_forward, positive_forward, negative_forward)
                 epoch_train_loss += loss
             # Get gradients of loss wrt the weights.
             gradients = tape.gradient(loss, model.trainable_weights)
@@ -74,7 +80,7 @@ def train(loss_function):
             optimizer.apply_gradients(zip(gradients, model.trainable_weights))
         epoch_test_loss = tf.convert_to_tensor(0, dtype=tf.float32)
         for test_batch in testing_set:
-            test_cos_sim = cos_sim(test_batch[0], test_batch[1])
+            test_cos_sim = loss_function(test_batch[0], test_batch[1])
             cos_sim_as_actual = tf.math.abs(test_batch[2] - test_cos_sim)
             test_batch_loss = tf.reduce_mean(cos_sim_as_actual)
             epoch_test_loss += test_batch_loss
@@ -94,8 +100,8 @@ def train(loss_function):
 
 
 if __name__ == '__main__':
-    if args.lf == 'cos':
+    if args.loss_function == 'cos':
         loss_function = cos_triplet_loss
-    elif args.lf == 'euc':
+    elif args.loss_function == 'euc':
         loss_function = euc_triplet_loss
     train(loss_function=loss_function)
