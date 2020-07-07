7990bc44d78b29bf0221e854b5a5ecefbe402e3c
Cooper W Linsky <cwl4602@sporcsubmit.rc.rit.edu>
2020-07-06 22:36:45 -0400

    Virtual environment can be setup with setup_venv.sh

---
+++ setup_venv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install numpy
+pip install opencv-python
+pip install tensorflow
+pip install Keras==2.4.2
+pip install Pillow
+pip install scikit-image
+pip install efficientnet
+pip install Keras-Applications
+deactivate
